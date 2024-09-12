a087528043272d34e9c42449f3e1ea9fd700029a
RohanKVerma <rohan.verma@axeno.co>
2024-09-12 14:03:57 +0530

    Added smooth scroll

--- Code/js/script.js
+++ Code/js/script.js
@@ -188,3 +188,31 @@ gsap.to('.end_char',{
       y:0,
       opacity:1
 })
+
+const lenis = new Lenis()
+
+lenis.on('scroll', (e) => {
+  console.log(e)
+})
+
+function raf(time) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf)
+
+
+// const lenis = new Lenis()
+
+lenis.on('scroll', (e) => {
+  console.log(e)
+})
+
+lenis.on('scroll', ScrollTrigger.update)
+
+gsap.ticker.add((time)=>{
+  lenis.raf(time * 1000)
+})
+
+gsap.ticker.lagSmoothing(0)
